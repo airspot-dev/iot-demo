f70ec5736bbb0ef560b5452f9844cbf3611e1e97
LorenzoBtz <lorenzo@airspot.tech>
2020-12-21 13:04:48 +0100

    implement fleet folders creation on new model create

--- rulesets/django-orm-to-gcs/ruleset_functions/__init__.py
+++ rulesets/django-orm-to-gcs/ruleset_functions/__init__.py
@@ -1,10 +1,16 @@
-from cloudstorage.drivers.google import GoogleStorageDriver
+from google.cloud import storage
 from krules_core.base_functions import RuleFunctionBase
 
 
-class CreateFleetGCSFolder(RuleFunctionBase):
+def create_gcs_folder(bucket, name):
+    blob = bucket.blob(name)
+    blob.upload_from_string('', content_type='application/x-www-form-urlencoded;charset=UTF-8')
+
+
+class CreateFleetGCSFolders(RuleFunctionBase):
+
     def execute(self, bucket, fleet):
-        driver = GoogleStorageDriver()
-        bucket = driver.get_container(bucket)
-        bucket.upload_blob("%s/import/class-a/" % fleet)
-        bucket.upload_blob("%s/import/class-b/" % fleet)
+        client = storage.Client()
+        bucket = client.get_bucket(bucket)
+        create_gcs_folder(bucket, "%s/import/class-a/" % fleet)
+        create_gcs_folder(bucket, "%s/import/class-b/" % fleet)
