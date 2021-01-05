3a1967091699c1ecf33c1b4dc4d68ce82cdbecc9
LorenzoBtz <lorenzo@airspot.tech>
2021-01-05 12:22:15 +0100

    add README and implement on-gcs-csv-upload

---
+++ rulesets/procevents-got-errors-reply/README.md
@@ -0,0 +1,3 @@
+# procevents-got-errors-reply
+
+responds to the procevent events and, if got errors, send back it to the original ruleset
--- support/django/django-site/device_manager/models.py
+++ support/django/django-site/device_manager/models.py
@@ -12,24 +12,44 @@ class Fleet(models.Model):
     def __str__(self):
         return self.name
 
+    def delete(self, using=None, keep_parents=False):
+        Device.objects.filter(fleet=self.name).delete()
+        super().delete(using, keep_parents)
+
 
 class Device(models.Model):
 
     id = models.CharField(max_length=255, primary_key=True)
     device_class = models.CharField(max_length=255, verbose_name="class")
     provisioning_data = models.JSONField()
-    fleet = models.ForeignKey(Fleet, on_delete=models.deletion.CASCADE)
+    fleet = models.CharField(max_length=255)
     status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.id
 
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if Fleet.objects.filter(name=self.fleet) == 0:
+            raise Exception("Fleet with name %s does not exists" % self.fleet)
+        super().save(force_insert, force_update, using, update_fields)
+
+    def delete(self, using=None, keep_parents=False):
+        ReceivedData.objects.filter(device=self.id).delete()
+        super().delete(using, keep_parents)
+
 
 class ReceivedData(models.Model):
 
-    device = models.ForeignKey(Device, on_delete=models.deletion.CASCADE)
+    device = models.CharField(max_length=255)
     data = models.JSONField()
     timestamp = models.DateTimeField()
 
     class Meta:
         verbose_name_plural = "received data"
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if Device.objects.filter(name=self.device) == 0:
+            raise Exception("Device with name %s does not exists" % self.device)
+        super().save(force_insert, force_update, using, update_fields)
