2a32e43730cc2d5cd8180c832004203861a295d7
LorenzoBtz <lorenzo@airspot.tech>
2021-01-05 12:52:10 +0100

    fix ruleset template

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -56,12 +56,6 @@ ruleset_py = """
 from krules_core.base_functions import *
 from krules_core import RuleConst as Const
 
-rulename = Const.RULENAME
-subscribe_to = Const.SUBSCRIBE_TO
-ruledata = Const.RULEDATA
-filters = Const.FILTERS
-processing = Const.PROCESSING
-
 from krules_core.providers import proc_events_rx_factory
 from krules_env import publish_proc_events_errors, publish_proc_events_all  #, publish_proc_events_filtered
 
@@ -70,7 +64,15 @@ try:
 except ImportError:
     # for local development
     from .ruleset_functions import *
-    
+
+
+rulename = Const.RULENAME
+subscribe_to = Const.SUBSCRIBE_TO
+ruledata = Const.RULEDATA
+filters = Const.FILTERS
+processing = Const.PROCESSING
+
+
 # proc_events_rx_factory().subscribe(
 #   on_next=publish_proc_events_all,
 # )
